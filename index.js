caadb9510146c5ef0a34f88d6b4d5a55317aabb7
sharifrayhan <sharifrayhan.official@gmail.com>
2023-12-21 12:30:30 +0600

    basic structure setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create Express app
+const app = express();
+
+// Middleware
+app.use(cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    credentials: true
+  }))
+app.use(express.json());
+app.use(cookieParser());
+
+// Connect to MongoDB
+const connectDB = require('./src/config/mongoose');
+connectDB();
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Wandelo Server!')
+  })
+
+
+
+
+
